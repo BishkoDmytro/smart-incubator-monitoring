a2adff94e69448eda084413c2eab32f84ec4cc23
BishkoDmytro <dimabishko1892@gmail.com>
2024-11-19 17:12:55 +0200

    remove db

--- app.py
+++ app.py
@@ -311,6 +311,4 @@ admin.add_view(MyAdminView(name='Керування даними', endpoint='adm
 
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()  # Створення таблиць у базі даних
     app.run(debug=True, host='0.0.0.0')
